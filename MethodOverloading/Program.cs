dcf0e886902426bc07e9e4327213fa45721a509a
navpreet <navpreetbatth2.nb@gmail.com>
2024-03-04 13:20:58 -0800

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           Console.WriteLine(Add(2,2, true));
+
+        }
+        public static int Add(int number1, int number2)
+        {
+            int answer = number1 + number2;
+            return answer;
+        }
+        public static decimal Add(decimal number1, decimal number2) 
+        {
+            decimal answer = number1 + number2;
+            return answer;
+        }
+        public static string Add(int number1, int number2, bool isMoney)
+        {
+            int answer = number1 + number2;
+
+            if (isMoney == true && answer > 1)
+            {
+                return $"{answer} dollars";
+            }
+            else
+            {
+                return $"{answer} dollar";
+                // return answer.ToString(); // to convert ansewr to string
+            }
         }
     }
 }
